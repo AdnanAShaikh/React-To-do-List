0b5b230a48bd4a9ce88c238207ab42e27dbf7509
AdnanAShaikh <shaikhadnan862@gmail.com>
2024-05-22 06:09:17 +0530

    localeStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Logo } from "./Logo";
 import { Form } from "./Form";
 import { PackingList } from "./PackingList";
 import { Stats } from "./Stats";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+// ];
 
 export default function App() {
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const storedItems = localStorage.getItem("items");
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
